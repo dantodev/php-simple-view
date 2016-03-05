461899652699a950a05c6c340eda71ed3d88ef91
Daniel Kahl <kontakt@daniel-kahl.de>
2016-03-05 21:02:10 +0100

    add support for multiple view paths

--- src/ViewRenderer.php
+++ src/ViewRenderer.php
@@ -2,27 +2,32 @@
 
 class ViewRenderer {
 
-  private $view_path;
+  private $_view_paths = [];
 
-  public function __construct($templatePath = "")
+  public function __construct($view_paths = [])
   {
-    $this->view_path = $templatePath;
+    $this->addViewPath($view_paths);
+  }
+
+  public function addViewPath($view_paths)
+  {
+    $this->_view_paths = is_array($view_paths) ? $view_paths : [$view_paths];
   }
 
   public function render($file, $data = [])
   {
-    if (!is_file($this->view_path . $file)) {
-      throw new \RuntimeException("View `$file` does not exist.");
+    foreach ($this->_view_paths as $path) {
+      if (is_file($path.$file)) {
+        $run = function ($file, $data) {
+          extract($data);
+          include $file;
+        };
+        ob_start();
+        $run($path.$file, $data);
+        return ob_get_clean();
+      }
     }
-
-    $run = function ($file, $data) {
-      extract($data);
-      include $file;
-    };
-
-    ob_start();
-    $run($this->view_path.$file, $data);
-    return ob_get_clean();
+    throw new \RuntimeException("View `$file` does not exist.");
   }
 
 }
