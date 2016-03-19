e166231575d920f5778610abf8178d3eec4addfc
Daniel Kahl <kontakt@daniel-kahl.de>
2016-03-20 00:05:01 +0100

    * add unit test

--- tests/SimpleViewTest.php
+++ tests/SimpleViewTest.php
@@ -2,6 +2,10 @@
 
 class SimpleViewTest extends \PHPUnit_Framework_TestCase {
 
-  //
+  public function test()
+  {
+    $renderer = new ViewRenderer(['view_path' => __DIR__.'/testview/']);
+    $this->assertEquals('<div>bar</div>', $renderer->render('testview.php', ['foo'=> 'bar']));
+  }
 
 }
