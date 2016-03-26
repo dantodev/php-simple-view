233ef8542b15f312af5a79775b540c64238796bc
Daniel Kahl <kontakt@daniel-kahl.de>
2016-03-27 00:06:51 +0100

    * php5.6 support

--- tests/SimpleViewTest.php
+++ tests/SimpleViewTest.php
@@ -5,7 +5,9 @@ class SimpleViewTest extends \PHPUnit_Framework_TestCase {
   public function test()
   {
     $renderer = new ViewRenderer(__DIR__.'/testviews/');
-    $this->assertEquals('<div>bar</div>', $renderer->render('testview.php', ['foo'=> 'bar']));
+    $obj = new \stdClass();
+    $obj->foo2 = "bar2";
+    $this->assertEquals('<div>bar</div>', $renderer->render('testview.php', ['foo'=> 'bar'], $obj));
   }
 
 }
