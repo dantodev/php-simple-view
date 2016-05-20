e4d732279dfc8c2bf7f0221dfc34a9f22eb6786d
daniel <kontakt@daniel-kahl.de>
2016-05-20 16:09:21 +0200

    add PSR-2 compatibility #2

--- src/ViewRenderer.php
+++ src/ViewRenderer.php
@@ -1,57 +1,58 @@
 <?php namespace Dtkahl\SimpleView;
 
-class ViewRenderer {
+class ViewRenderer
+{
 
-  private $_view_paths = [];
+    private $_view_paths = [];
 
-  /**
-   * ViewRenderer constructor.
-   * @param string|string[] $view_paths
-   */
-  public function __construct($view_paths = [])
-  {
-    $this->addViewPath($view_paths);
-  }
-
-  /**
-   * @param string|string[] $view_paths
-   * @return $this
-   */
-  public function addViewPath($view_paths)
-  {
-    $this->_view_paths = array_merge($this->_view_paths, (array) $view_paths);
-    return $this;
-  }
-
-  /**
-   * @param string $file
-   * @param array $data
-   * @return string
-   */
-  public function render($file, array $data = [])
-  {
-    foreach ($this->_view_paths as $path) {
-      if (is_file($path.$file)) {
-        $run = function ($file, $data) {
-          extract($data);
-          include $file;
-        };
-        ob_start();
-        $run($path.$file, $data);
-        return ob_get_clean();
-      }
+    /**
+     * ViewRenderer constructor.
+     * @param string|string[] $view_paths
+     */
+    public function __construct($view_paths = [])
+    {
+        $this->addViewPath($view_paths);
     }
-    throw new \RuntimeException("View `$file` does not exist.");
-  }
 
-  /**
-   * @param string $file
-   * @param array $data
-   * @return string
-   */
-  public function __invoke($file, array $data = [])
-  {
-    return $this->render($file, $data);
-  }
+    /**
+     * @param string|string[] $view_paths
+     * @return $this
+     */
+    public function addViewPath($view_paths)
+    {
+        $this->_view_paths = array_merge($this->_view_paths, (array)$view_paths);
+        return $this;
+    }
+
+    /**
+     * @param string $file
+     * @param array $data
+     * @return string
+     */
+    public function render($file, array $data = [])
+    {
+        foreach ($this->_view_paths as $path) {
+            if (is_file($path . $file)) {
+                $run = function ($file, $data) {
+                    extract($data);
+                    include $file;
+                };
+                ob_start();
+                $run($path . $file, $data);
+                return ob_get_clean();
+            }
+        }
+        throw new \RuntimeException("View `$file` does not exist.");
+    }
+
+    /**
+     * @param string $file
+     * @param array $data
+     * @return string
+     */
+    public function __invoke($file, array $data = [])
+    {
+        return $this->render($file, $data);
+    }
 
 }
--- tests/SimpleViewTest.php
+++ tests/SimpleViewTest.php
@@ -1,14 +1,15 @@
 <?php namespace Dtkahl\SimpleView;
 
-class SimpleViewTest extends \PHPUnit_Framework_TestCase {
+class SimpleViewTest extends \PHPUnit_Framework_TestCase
+{
 
-  public function test()
-  {
-    $renderer = new ViewRenderer(__DIR__.'/testviews/');
-    $obj = new \stdClass();
-    $obj->foo2 = "bar2";
-    $this->assertEquals('<div>bar</div>', $renderer->render('testview.php', ['foo'=> 'bar'], $obj));
-    $this->assertEquals('<div>bar</div>', $renderer('testview.php', ['foo'=> 'bar'], $obj));
-  }
+    public function test()
+    {
+        $renderer = new ViewRenderer(__DIR__ . '/testviews/');
+        $obj = new \stdClass();
+        $obj->foo2 = "bar2";
+        $this->assertEquals('<div>bar</div>', $renderer->render('testview.php', ['foo' => 'bar'], $obj));
+        $this->assertEquals('<div>bar</div>', $renderer('testview.php', ['foo' => 'bar'], $obj));
+    }
 
 }
